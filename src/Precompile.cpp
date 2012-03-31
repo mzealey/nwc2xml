de734582f125b8d135b4e54abce1fce107882e4f
juria90 <juria90@gmail.com>
2012-03-30 23:54:41 -0700

    Add vc8 project

--- src/Precompile.cpp
+++ src/Precompile.cpp
@@ -1,6 +1,6 @@
-// stdafx.cpp : source file that includes just the standard includes
+// Precompile.cpp : source file that includes just the standard includes
 //	nwc2xml.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
+//	Precompile.obj will contain the pre-compiled type information
 
 #include "Precompile.h"
 
